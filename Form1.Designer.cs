aca7bd0f03e15ebf08662f5c935d68cd74561e03
Арсений <drars3883@icloud.com>
2024-04-01 12:24:48 +0800

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace labb3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(21, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(273, 27);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(21, 175);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(273, 27);
+            this.textBox2.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(334, 20);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(151, 28);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(334, 175);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(151, 28);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 97);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(133, 97);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(252, 97);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 29);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "*";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(380, 97);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(94, 29);
+            this.button4.TabIndex = 7;
+            this.button4.Text = ">/<";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(21, 351);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(273, 27);
+            this.textBox3.TabIndex = 8;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(463, 350);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(151, 28);
+            this.comboBox3.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(268, 228);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 20);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Результат:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(284, 271);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 20);
+            this.label3.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(177, 410);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 20);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Результат:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(261, 410);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 20);
+            this.label5.TabIndex = 14;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(322, 349);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(112, 29);
+            this.button5.TabIndex = 15;
+            this.button5.Text = "перевести в";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private TextBox textBox3;
+        private ComboBox comboBox3;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Button button5;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,169 @@
+using CustomNumberSystem;
+
+namespace labb3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // Добавляем элементы в выпадающие списки
+            comboBox1.Items.AddRange(Enum.GetValues(typeof(NumberSystem)).Cast<object>().ToArray());
+            comboBox2.Items.AddRange(Enum.GetValues(typeof(NumberSystem)).Cast<object>().ToArray());
+            comboBox3.Items.AddRange(Enum.GetValues(typeof(NumberSystem)).Cast<object>().ToArray());
+
+            // Выбираем первый элемент в каждом списке по умолчанию
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
+            comboBox3.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string number1Text = textBox1.Text;
+            string number2Text = textBox2.Text;
+            // Получаем выбранные системы счисления из выпадающих списков
+            NumberSystem system1 = (NumberSystem)comboBox1.SelectedItem;
+            NumberSystem system2 = (NumberSystem)comboBox2.SelectedItem;
+            // Создаем объекты CustomNumber
+            CustomNumber num1 = new CustomNumber(number1Text, system1);
+            CustomNumber num2 = new CustomNumber(number2Text, system2);
+            // Выполняем операции
+            CustomNumber sum = num1.Add(num2);
+            label3.Text = $"Сумма: {sum.GetValue()}";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string number1Text = textBox1.Text;
+            string number2Text = textBox2.Text;
+            // Получаем выбранные системы счисления из выпадающих списков
+            NumberSystem system1 = (NumberSystem)comboBox1.SelectedItem;
+            NumberSystem system2 = (NumberSystem)comboBox2.SelectedItem;
+            // Создаем объекты CustomNumber
+            CustomNumber num1 = new CustomNumber(number1Text, system1);
+            CustomNumber num2 = new CustomNumber(number2Text, system2);
+            // Выполняем операции
+            CustomNumber sum = num1.Subtract(num2);
+            label4.Text = $"Разница: {sum.GetValue()}";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string number1Text = textBox1.Text;
+            string number2Text = textBox2.Text;
+            // Получаем выбранные системы счисления из выпадающих списков
+            NumberSystem system1 = (NumberSystem)comboBox1.SelectedItem;
+            NumberSystem system2 = (NumberSystem)comboBox2.SelectedItem;
+            // Создаем объекты CustomNumber
+            CustomNumber num1 = new CustomNumber(number1Text, system1);
+            CustomNumber num2 = new CustomNumber(number2Text, system2);
+            // Выполняем операции
+            CustomNumber sum = num1.Multiply(num2);
+            label4.Text = $"Произведение: {sum.GetValue()}";
+        }
+    }
+}
+namespace CustomNumberSystem
+{
+    public enum NumberSystem
+    {
+        Binary,
+        Octal,
+        Decimal,
+        Hexadecimal
+    }
+
+    public class CustomNumber
+    {
+        private string _value;
+        private NumberSystem _numberSystem;
+
+        public CustomNumber(string value, NumberSystem numberSystem)
+        {
+            _value = value;
+            _numberSystem = numberSystem;
+        }
+
+        public CustomNumber Add(CustomNumber other)
+        {
+            // Convert both numbers to decimal
+            int thisDecimal = ConvertToDecimal();
+            int otherDecimal = other.ConvertToDecimal();
+
+            // Add and convert result back to original number system
+            int resultDecimal = thisDecimal + otherDecimal;
+            return new CustomNumber(ConvertFromDecimal(resultDecimal), _numberSystem);
+        }
+
+        public CustomNumber Subtract(CustomNumber other)
+        {
+            // Convert both numbers to decimal
+            int thisDecimal = ConvertToDecimal();
+            int otherDecimal = other.ConvertToDecimal();
+
+            // Subtract and convert result back to original number system
+            int resultDecimal = thisDecimal - otherDecimal;
+            return new CustomNumber(ConvertFromDecimal(resultDecimal), _numberSystem);
+        }
+
+        public CustomNumber Multiply(CustomNumber other)
+        {
+            // Convert both numbers to decimal
+            int thisDecimal = ConvertToDecimal();
+            int otherDecimal = other.ConvertToDecimal();
+
+            // Multiply and convert result back to original number system
+            int resultDecimal = thisDecimal * otherDecimal;
+            return new CustomNumber(ConvertFromDecimal(resultDecimal), _numberSystem);
+        }
+
+        public bool IsGreaterThan(CustomNumber other)
+        {
+            // Convert both numbers to decimal for comparison
+            int thisDecimal = ConvertToDecimal();
+            int otherDecimal = other.ConvertToDecimal();
+
+            return thisDecimal > otherDecimal;
+        }
+
+        public string GetValue()
+        {
+            return _value;
+        }
+
+        private int ConvertToDecimal()
+        {
+            switch (_numberSystem)
+            {
+                case NumberSystem.Binary:
+                    return Convert.ToInt32(_value, 2);
+                case NumberSystem.Octal:
+                    return Convert.ToInt32(_value, 8);
+                case NumberSystem.Decimal:
+                    return Convert.ToInt32(_value);
+                case NumberSystem.Hexadecimal:
+                    return Convert.ToInt32(_value, 16);
+                default:
+                    throw new InvalidOperationException("Unknown number system");
+            }
+        }
+
+        private string ConvertFromDecimal(int decimalValue)
+        {
+            switch (_numberSystem)
+            {
+                case NumberSystem.Binary:
+                    return Convert.ToString(decimalValue, 2);
+                case NumberSystem.Octal:
+                    return Convert.ToString(decimalValue, 8);
+                case NumberSystem.Decimal:
+                    return decimalValue.ToString();
+                case NumberSystem.Hexadecimal:
+                    return Convert.ToString(decimalValue, 16);
+                default:
+                    throw new InvalidOperationException("Unknown number system");
+            }
+        }
+    }
+}
